129d1a771694d141e2c7a564b55a051d8190c1e4
harshit8398 <harshit8398@gmail.com>
2017-10-11 12:44:32 +0530

    Initial commit

---
+++ public_static/client.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Harshit Gupta on 14-07-2017.
+ */
+let socket = io();
+
+$(function () {
+
+    let newmsg = $('#newmsg');
+    let sendmsgbtn = $('#sendmsg');
+    let msglist = $('#msglist');
+    let loginbtn = $('#login');
+    let username = $('#username');
+    let loginframe = $('#login-container');
+    let chatframe = $('#chat-container');
+
+    if (sessionStorage.getItem('username')) {
+        socket.emit('login', sessionStorage.getItem('username'))
+    }
+
+    loginbtn.click(function () {
+        socket.emit('login', username.val())
+    });
+
+        socket.on('logged_in', (data) => {
+            chatframe.show();
+    loginframe.hide();
+    console.log('logged_in');
+    for (let chat of data.chats) {
+        msglist.append($(`<li>${chat}</li>`))
+    }
+    sessionStorage.setItem('username', data.username)
+});
+
+    sendmsgbtn.click(function () {
+        socket.emit('new_message', newmsg.val());
+        newmsg.val(' ');
+    });
+
+    socket.on('recv_message', (data) => {
+        console.log('msg recvd');
+    msglist.append($(`<li>${data}</li>`));
+});
+
+    socket.on('disconnect', () => {
+        chatframe.hide();
+        loginframe.show();
+        msglist.empty();
+});
+
+});
---
+++ server.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Harshit Gupta on 14-07-2017.
+ */
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+let users = {};
+let chats = [];
+
+app.use('/', express.static(__dirname + "/public_static"));
+app.get('/hello', (r,s) => s.send("hello"));
+
+io.on('connection', (socket) => {
+    console.log("New client connected");
+
+socket.on('login', (username) => {
+    console.log('User logged in : ' + socket.id);
+users[socket.id] = username;
+socket.emit('logged_in', {username, chats});
+});
+
+
+socket.on('new_message', (data) => {
+    let chat = users[socket.id] + ": " + data;
+    chats.push(chat);
+console.log('msg received');
+io.emit('recv_message', chat);
+});
+
+});
+
+
+server.listen(3245, function () {
+    console.log("Server started on http://localhost:3245");
+});
